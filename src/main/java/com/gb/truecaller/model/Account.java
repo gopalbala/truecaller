278b49f943f6c7714be847bc9c5d3b9a0815cdc1
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-20 00:17:34 +0530

    updated Account

--- src/main/java/com/gb/truecaller/model/Account.java
+++ src/main/java/com/gb/truecaller/model/Account.java
@@ -1,5 +1,8 @@
 package com.gb.truecaller.model;
 
+import com.gb.truecaller.model.common.PersonalInfo;
+import com.gb.truecaller.model.common.SocialInfo;
+import com.gb.truecaller.model.common.Tag;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +12,12 @@ import java.time.LocalDateTime;
 @Setter
 public abstract class Account {
     private String id;
-    private String email;
+    private String phoneNumber;
     private String userName;
     private String password;
     private LocalDateTime lastAccessed;
+    private Tag tag;
     private Contact contact;
+    private PersonalInfo personalInfo;
+    private SocialInfo socialInfo;
 }

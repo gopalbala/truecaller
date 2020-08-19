c01f22b53f67c521066979552c03a35438edad2a
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-19 23:44:07 +0530

    added social info

---
+++ src/main/java/com/gb/truecaller/model/common/SocialInfo.java
@@ -0,0 +1,12 @@
+package com.gb.truecaller.model.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SocialInfo {
+    String companyName;
+    String url;
+    Address companyAddress;
+}

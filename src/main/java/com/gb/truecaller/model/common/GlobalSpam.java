f1cb3f67355f6c1a7997e6d178b412d5583c45f1
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-22 10:48:28 +0530

    Made GlobalSpam as singleton

--- src/main/java/com/gb/truecaller/model/common/GlobalSpam.java
+++ src/main/java/com/gb/truecaller/model/common/GlobalSpam.java
@@ -2,17 +2,13 @@ package com.gb.truecaller.model.common;
 
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
-import lombok.Getter;
-import lombok.Setter;
 import orestes.bloomfilter.CountingBloomFilter;
 import orestes.bloomfilter.FilterBuilder;
 
 import java.nio.charset.Charset;
 
-@Getter
-@Setter
 public class GlobalSpam {
-    private BloomFilter<String> blockedContacts =
+    private BloomFilter<String> globalBlocked =
             BloomFilter.create(Funnels.stringFunnel(Charset.forName("UTF-8")),
                     10_000_000);
 
@@ -20,4 +16,18 @@ public class GlobalSpam {
             new FilterBuilder(10_000_000, .01)
                     .buildCountingBloomFilter(
                     );
+
+    private GlobalSpam() {
+    }
+
+    public static GlobalSpam INSTANCE = new GlobalSpam();
+
+    public void reportSpam(String number) {
+        if (globalSpam.getEstimatedCount(number) > 10000) {
+            globalBlocked.put(number);
+        } else {
+            globalSpam.add(number);
+        }
+
+    }
 }

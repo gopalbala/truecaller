1fcd86b6043347ffa2b657d6f283d36202be110c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-22 10:46:57 +0530

    Added ContactDoesNotExistsException

---
+++ src/main/java/com/gb/truecaller/exception/ContactDoesNotExistsException.java
@@ -0,0 +1,7 @@
+package com.gb.truecaller.exception;
+
+public class ContactDoesNotExistsException extends Exception {
+    public ContactDoesNotExistsException(String message) {
+        super(message);
+    }
+}

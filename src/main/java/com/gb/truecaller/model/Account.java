dfce13c86a2fd8553eaa2a83ab9c9ffbdce86dc8
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-23 12:34:05 +0530

    moved contats and blocked contacts to account

--- src/main/java/com/gb/truecaller/model/Account.java
+++ src/main/java/com/gb/truecaller/model/Account.java
@@ -6,8 +6,15 @@ import com.gb.truecaller.model.common.SocialInfo;
 import com.gb.truecaller.model.common.Tag;
 import lombok.Getter;
 import lombok.Setter;
+import orestes.bloomfilter.CountingBloomFilter;
+import orestes.bloomfilter.FilterBuilder;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.gb.truecaller.model.common.Constant.*;
 
 @Getter
 @Setter
@@ -22,4 +29,41 @@ public abstract class Account {
     private PersonalInfo personalInfo;
     private SocialInfo socialInfo;
     private Business business;
+    private UserType userType;
+    private Map<String, User> contacts;
+    private CountingBloomFilter<String> blockedContacts;
+
+    public Account( UserType userType, String userName, String password,
+                   String email, String phoneNumber, String countryCode) {
+        this.id = UUID.randomUUID().toString();
+        this.userType = userType;
+        this.userName = userName;
+        this.password = password;
+        this.contact = new Contact();
+        contact.setEmail(email);
+        contact.setPhone(phoneNumber);
+        contact.setCountryCode(countryCode);
+        init(userType);
+    }
+
+    private void init(UserType userType) {
+        switch (userType){
+            case FREE:
+                contacts = new HashMap<>(MAX_FREE_USER_CONTACTS);
+                blockedContacts = new FilterBuilder(MAX_FREE_USER_BLOCKED_CONTACTS, .01)
+                        .buildCountingBloomFilter();
+                break;
+            case GOLD:
+                contacts = new HashMap<>(MAX_GOLD_USER_CONTACTS);
+                blockedContacts = new FilterBuilder(MAX_GOLD_USER_BLOCKED_CONTACTS, .01)
+                        .buildCountingBloomFilter();
+                break;
+
+            case PLATINUM:
+                contacts = new HashMap<>(MAX_PLATINUM_USER_CONTACTS);
+                blockedContacts = new FilterBuilder(MAX_PLATINUM_USER_BLOCKED_CONTACTS, .01)
+                        .buildCountingBloomFilter();
+                break;
+        }
+    }
 }

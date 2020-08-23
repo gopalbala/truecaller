2d3d3cce0bed6840b1e10ce825b9df038e6bd11f
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-23 23:05:32 +0530

    added constructors

--- src/main/java/com/gb/truecaller/model/common/Contact.java
+++ src/main/java/com/gb/truecaller/model/common/Contact.java
@@ -9,4 +9,16 @@ public class Contact {
     private String countryCode;
     private String phone;
     private String email;
+    public Contact(String phone) {
+        this.phone = phone;
+    }
+    public Contact(String phone, String email) {
+        this.phone = phone;
+        this.email = email;
+    }
+    public Contact(String countryCode, String phone, String email) {
+        this.countryCode = countryCode;
+        this.phone = phone;
+        this.email = email;
+    }
 }

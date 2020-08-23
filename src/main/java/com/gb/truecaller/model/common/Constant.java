57401020a23e7e1a811a1c36756d2da8b8a63fb9
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-23 12:17:34 +0530

    added Constant.java

---
+++ src/main/java/com/gb/truecaller/model/common/Constant.java
@@ -0,0 +1,12 @@
+package com.gb.truecaller.model.common;
+
+public class Constant {
+    public static final int MAX_FREE_USER_CONTACTS = 10_000;
+    public static final int MAX_FREE_USER_BLOCKED_CONTACTS = 100;
+
+    public static final int MAX_GOLD_USER_CONTACTS = 100_000;
+    public static final int MAX_GOLD_USER_BLOCKED_CONTACTS = 1000;
+
+    public static final int MAX_PLATINUM_USER_CONTACTS = Integer.MAX_VALUE;
+    public static final int MAX_PLATINUM_USER_BLOCKED_CONTACTS = 100_000;
+}

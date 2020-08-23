a58787ce66baf860bd9ba547a68797cd084541e4
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-23 16:39:40 +0530

    added test cases

--- src/main/java/com/gb/truecaller/TrueCallerApplication.java
+++ src/main/java/com/gb/truecaller/TrueCallerApplication.java
@@ -1,7 +1,79 @@
 package com.gb.truecaller;
 
-public class TrueCallerApplication {
-    public static void main(String[] args) {
+import com.gb.truecaller.exception.ContactsExceededException;
+import com.gb.truecaller.model.Account;
+import com.gb.truecaller.model.User;
+import com.gb.truecaller.model.UserType;
+import com.gb.truecaller.model.tries.ContactTrie;
+import com.gb.truecaller.model.tries.TrieNode;
 
+import java.util.List;
+
+public class TrueCallerApplication {
+    public static void main(String[] args) throws ContactsExceededException {
+
+        //Test case 1: Create user and register
+        Account account1 = new User();
+        account1.register(UserType.FREE,"u1","pwd",
+                "u1@email.com","6826999256", "91");
+
+        // Test case 2: Add contacts to user
+        account1.addConcat(new User("9140107431","mahadev"));
+        account1.addConcat(new User("8558101117","govind", "hs"));
+        account1.addConcat(new User("8723937942","gopala"));
+        account1.addConcat(new User("7070063864","mahesha"));
+        account1.addConcat(new User("6610448270","parvathi"));
+        account1.addConcat(new User("7336175457","parameshwari"));
+        account1.addConcat(new User("7202250272","narayan"));
+        account1.addConcat(new User("7859999997","lakshmi"));
+        account1.addConcat(new User("9653498522","ganesh", "kumar"));
+        account1.addConcat(new User("7277115893","ganapathy"));
+        account1.addConcat(new User("9495010564","Bhrama"));
+        account1.addConcat(new User("9844296241","Saraswathi"));
+        account1.addConcat(new User("7917949575","Veena"));
+
+        //Test case 3: check added contacts count
+        System.out.println(account1.getContacts().size());
+
+        //Test case 4: search for contacts by name
+        List<String> names = ContactTrie.CONTACT_TRIE.allWordsWithPrefix("par");
+        for (String n: names) {
+            System.out.println(n);
+        }
+
+        //Test case 5: search for contacts by name
+        System.out.println("*****");
+        names = ContactTrie.CONTACT_TRIE.allWordsWithPrefix("go");
+        for (String n: names) {
+            System.out.println(n);
+        }
+
+        //Test case 6: search for contacts by phone
+        System.out.println("*****");
+        names = ContactTrie.CONTACT_TRIE.allWordsWithPrefix("9");
+        for (String n: names) {
+            System.out.println(n);
+        }
+
+        //Test case 6a: search for contacts by phone
+        System.out.println("*****");
+        names = ContactTrie.CONTACT_TRIE.allWordsWithPrefix("9");
+        for (String n: names) {
+            System.out.println(n);
+        }
+
+        //Test case 6b: search for contacts by phone
+        System.out.println("*****");
+        names = ContactTrie.CONTACT_TRIE.allWordsWithPrefix("72");
+        for (String n: names) {
+            System.out.println(n);
+        }
+
+        //Test case 6c: search for contacts by phone
+        System.out.println("*****");
+        names = ContactTrie.CONTACT_TRIE.allWordsWithPrefix("6610448270");
+        for (String n: names) {
+            System.out.println(n);
+        }
     }
 }
